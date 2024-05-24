12798e86bcc51932418a5cae5d70b4bba97b7cff
dodecahedron <moutalovkamilmus@gmail.com>
2024-05-24 22:02:13 +0500

    Completed console app.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the number of cards to pick: ");
+            string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+            else Console.WriteLine("Ti daun?");
         }
     }
 }
